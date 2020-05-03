20c1fdbd6b142671a5a79d89d29c6fdf77c5c618
Nathaniel Ludwig <nathanielludwig1@gmail.com>
2020-05-02 22:08:23 -0500

    added spotify authorization flow

---
+++ SpotifyRecommender/SpotifyRecommender-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  SpotifyRecommender-Bridging-Header.h
+//  SpotifyRecommender
+//
+//  Created by 90303054 on 4/27/20.
+//  Copyright © 2020 90303054. All rights reserved.
+//
+#import <SpotifyiOS/SpotifyiOS.h>
+
--- SpotifyRecommender/ViewController.swift
+++ SpotifyRecommender/ViewController.swift
@@ -8,13 +8,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, SPTSessionManagerDelegate {
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
+    private let SpotifyClientID = "6fbaa216180b4898872744e61e14df49"
+    private let SpotifyRedirectURL = URL(string: "spotifyrecommender://spotify-login-callback")!
+    
+    lazy var configuration = SPTConfiguration(
+      clientID: SpotifyClientID,
+      redirectURL: SpotifyRedirectURL
+    )
+    
+    lazy var sessionManager: SPTSessionManager = {
+      if let tokenSwapURL = URL(string: "https://spotifyrecommender.herokuapp.com/api/token"),
+         let tokenRefreshURL = URL(string: "https://spotifyrecommender.herokuapp.com/api/refresh_token") {
+        self.configuration.tokenSwapURL = tokenSwapURL
+        self.configuration.tokenRefreshURL = tokenRefreshURL
+        self.configuration.playURI = ""
+      }
+      let manager = SPTSessionManager(configuration: self.configuration, delegate: self)
+      return manager
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func loginButtonPressed(_ sender: Any) {
+        let scope: SPTScope = [.playlistReadPrivate]
+        sessionManager.initiateSession(with: scope, options: .default)
+    }
+    
+    // MARK: - SPTSessionManagerDelegate
+    func sessionManager(manager: SPTSessionManager, didInitiate session: SPTSession) {
+        print("session connected")
+    }
+    
+    func sessionManager(manager: SPTSessionManager, didFailWith error: Error) {
+        print("session connect failed", error)
+    }
+    func sessionManager(manager: SPTSessionManager, didRenew session: SPTSession) {
+        print("session renewed")
+    }
+    
+    
 }
+
 
